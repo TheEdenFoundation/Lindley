858a31f532be2f1b36280115552e8896f972c707
Imad <hello@imadkazi.co.uk>
2025-01-11 19:09:13 +0000

    Added: Added strapi using postgres (currently local config)

---
+++ backend/src/api/salaah-time/routes/salaah-time.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * salaah-time router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::salaah-time.salaah-time');

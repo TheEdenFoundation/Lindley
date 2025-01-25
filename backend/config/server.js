5cfe6e8be8c468a67e041464b335047c154bd4e8
Imad <hello@imadkazi.co.uk>
2025-01-25 19:07:07 +0000

    Updated server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://admin.masjidly.co.uk"), // Add this line
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });

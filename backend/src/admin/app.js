a8b2b443e942ffd17b0899f0b3693deb5b4bf66b
Imad <hello@imadkazi.co.uk>
2025-01-25 21:31:14 +0000

    Updated branding

--- backend/src/admin/app.js
+++ backend/src/admin/app.js
@@ -1,11 +1,11 @@
 const config = {
-  translations: {
-    en: {
-      "app.components.LeftMenu.navbrand.title": "Shah Jalal Masjid",
-      "Auth.form.welcome.subtitle": "Log in to your Shah Jalal Masjid account",
-      "Auth.form.welcome.title": "Welcome to Shah Jalal Masjid!",
-    },
-  },
+  // translations: {
+  //   en: {
+  //     "app.components.LeftMenu.navbrand.title": "Shah Jalal Masjid",
+  //     "Auth.form.welcome.subtitle": "Log in to your Shah Jalal Masjid account",
+  //     "Auth.form.welcome.title": "Welcome to Shah Jalal Masjid!",
+  //   },
+  // },
 };
 
 const bootstrap = (app) => {

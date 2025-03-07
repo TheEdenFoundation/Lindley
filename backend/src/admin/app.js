3a4862c697b7aeb47fd1e23c8e9e02ab92580b03
Imad <hello@imadkazi.co.uk>
2025-03-07 15:08:21 +0000

    Update: Strapi Branding

--- backend/src/admin/app.js
+++ backend/src/admin/app.js
@@ -1,11 +1,11 @@
 const config = {
   translations: {
     en: {
-      "app.components.LeftMenu.navbrand.title": "Masjidly",
-      "Auth.form.welcome.subtitle": "Log in to your Masjidly account",
-      "Auth.form.welcome.title": "Welcome to Masjidly!",
+      "app.components.LeftMenu.navbrand.title": "The Eden Foundation Lindley",
+      "Auth.form.welcome.subtitle": "Log in to your TEF Lindley account",
+      "Auth.form.welcome.title": "Welcome to The Eden Foundation Lindley!",
       "Auth.form.register.subtitle":
-        "Credentials are only used to authenticate in Masjidly. All saved data will be stored in your database.",
+        "Credentials are only used to authenticate in TEF Lindley. All saved data will be stored in your database.",
     },
   },
 };

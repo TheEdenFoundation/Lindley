369730009bd9e77d649647a883b87ae82704f155
Imad <hello@imadkazi.co.uk>
2025-03-07 15:37:03 +0000

    Added: SendGrid mail proivder

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -14,4 +14,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "salaam@masjidly.co.uk",
+        defaultReplyTo: "salaam@masjidly.co.uk",
+      },
+    },
+  },
 });

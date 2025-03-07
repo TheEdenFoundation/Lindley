54ec678ef94cd91594abb5f2ed51b6252fcb9bd6
Imad <hello@imadkazi.co.uk>
2025-03-07 17:01:36 +0000

    Update: Email templatee

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,12 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
-// Read the HTML template file
-const forgotPasswordTemplate = fs.readFileSync(
-  path.join(__dirname, "../templates/emails/forgot-password.html"),
-  "utf8"
-);
-
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
@@ -24,7 +15,7 @@ module.exports = ({ env }) => ({
     replyTo: "salaam@masjidly.co.uk",
     emailTemplate: {
       subject: "TEF Lindley - Reset Password",
-      html: forgotPasswordTemplate,
+      html: "../templates/emails/forgot-password.html",
       text: `Reset your password by clicking this link: <%= URL %>?code=<%= TOKEN %>`,
     },
   },
